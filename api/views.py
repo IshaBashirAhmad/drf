30c279f2dce062f17fdc3907919185f216363876
isha <iishabashir@gmail.com>
2026-01-05 23:00:20 +0500

    feat: Implement EmployeeDetails API for retrieving, updating, and deleting employee records

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 # from django.shortcuts import render
 # from django.http import JsonResponse
+from django.http import Http404
 from student.models import Student
 from .serializers import StudentSerializer, EmpolyeeSerializer
 from rest_framework.response import Response
@@ -58,3 +59,24 @@ class EmployeeView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+class EmployeeDetails(APIView):
+    def get_object(self, pk):
+        try:
+            return Employee.objects.get(pk=pk)
+        except Employee.DoesNotExist:
+            raise Http404
+    def get(self, request, pk):
+        employee = self.get_object(pk)
+        serializer = EmpolyeeSerializer(employee)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    def put(self, request, pk):
+        employee = self.get_object(pk)
+        serializer = EmpolyeeSerializer(employee, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def delete(self, request, pk):
+        employee = self.get_object(pk)
+        employee.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
